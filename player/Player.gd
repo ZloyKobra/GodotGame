992758576a9e69d49a932a3a3be879ddc196b1d9
TOkotit <tokotit32@gmail.com>
2024-10-17 23:37:41 +0500

    потихоньку делаем

--- player/Player.gd
+++ player/Player.gd
@@ -1,18 +1,43 @@
-extends CharacterBody2D
+extends Sprite2D
 
-var direction = Vector2.ZERO
+@onready var tile_map = $"../TileMap"
+@onready var sprite_2d = $"Sprite2D"
+
+var is_moving = false
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("ui_left"):
-		direction.x -= 128
-	elif Input.is_action_just_pressed("ui_right"):
-		direction.x += 128
-	elif Input.is_action_just_pressed("ui_up"):
-		direction.y -= 128
-	elif Input.is_action_just_pressed("ui_down"):
-		direction.y += 128
+	if is_moving == false:
+		return
+	if global_position == sprite_2d.global_position:
+		is_moving = false
+		return
+	
+	sprite_2d.global_position = sprite_2d.global_position.move_toward(global_position, 1)
+	
+func _process(delta):
+	if is_moving:
+		return
+	
+	if Input.is_action_just_pressed("left"):
+		move(Vector2.LEFT)
+	elif Input.is_action_just_pressed("right"):
+		move(Vector2.RIGHT)
+	elif Input.is_action_just_pressed("up"):
+		move(Vector2.UP)
+	elif Input.is_action_just_pressed("down"):
+		move(Vector2.DOWN)
 
-	else:
-		direction.y = 0
-		direction.x = 0
-	move_and_collide(direction)
+func move(direction: Vector2):
+	var current_tile: Vector2i = tile_map.local_to_map(global_position)
+	var target_tile: Vector2i = Vector2i(
+		 current_tile.x + direction.x,
+		current_tile.y + direction.y
+	)
+	var tile_data: TileData = tile_map.get_cell_tile_data(0, target_tile)
+	
+	if tile_data.get_custom_data("walkable") == false:
+		return
+	is_moving = true
+	global_position = tile_map.map_to_local(target_tile)
+	sprite_2d.global_position = tile_map.map_to_local(current_tile)
+	
